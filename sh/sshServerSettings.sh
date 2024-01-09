854649291d3dab604da71a600b15ebdc06120f20
yutaishikawa526 <yutaishikawa526@gmail.com>
2024-01-10 01:33:15 +0900

    Create sshServerSettings.sh

---
+++ sh/sshServerSettings.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# sshのサーバー設定
+# 最終的には~/sshSettingsディレクトリにssh接続用の秘密鍵が残る
+
+## sshサーバーのインストール
+sudo apt -y update
+sudo apt -y install openssh-server
+
+mkdir ~/sshSettings
+cd ~/sshSettings
+
+## 自動起動設定
+sudo systemctl enable ssh
+
+## クライアント用秘密鍵、公開鍵の作成
+ssh-keygen -t rsa -b 2048  -C ''  -f id_rsa_ssh_main
+sudo touch /etc/ssh/authorized_keys
+cat id_rsa_ssh_main.pub | sudo sh -c 'cat - >> /etc/ssh/authorized_keys'
+sudo chmod 600 /etc/ssh/authorized_keys
+
+## 設定ファイルの修正
+{
+    echo 'Port 22'
+    echo '#ListenAddress 0.0.0.0'
+    echo ''
+    echo '# Logging'
+    echo 'LogLevel INFO'
+    echo ''
+    echo 'PasswordAuthentication no'
+    echo 'PermitRootLogin yes'
+    echo 'StrictModes yes'
+    echo 'MaxAuthTries 6'
+    echo 'MaxSessions 10'
+    echo ''
+    echo 'RSAAuthentication yes'
+    echo 'PubkeyAuthentication yes'
+    echo 'AuthorizedKeysFile /etc/ssh/authorized_keys'
+    echo ''
+    echo 'KbdInteractiveAuthentication no'
+    echo ''
+    echo 'UsePAM no'
+    echo ''
+    echo ''
+    echo '#AllowAgentForwarding yes'
+    echo '#AllowTcpForwarding yes'
+    echo '#GatewayPorts no'
+    echo 'X11Forwarding yes'
+    echo '#X11DisplayOffset 10'
+    echo '#X11UseLocalhost yes'
+    echo '#PermitTTY yes'
+    echo 'PrintMotd no'
+    echo '#PrintLastLog yes'
+    echo '#TCPKeepAlive yes'
+    echo '#PermitUserEnvironment no'
+    echo '#Compression delayed'
+    echo '#ClientAliveInterval 0'
+    echo '#ClientAliveCountMax 3'
+    echo '#UseDNS no'
+    echo '#PidFile /run/sshd.pid'
+    echo '#MaxStartups 10:30:100'
+    echo '#PermitTunnel no'
+    echo '#ChrootDirectory none'
+    echo '#VersionAddendum none'
+    echo '# Allow client to pass locale environment variables'
+    echo 'AcceptEnv LANG LC_*'
+    echo '# override default of no subsystems'
+    echo 'Subsystem sftp /usr/lib/openssh/sftp-server'
+} | sudo sh -c 'cat - > /etc/ssh/sshd_config'
+
+## 公開鍵削除
+rm id_rsa_ssh_main.pub
+
+## 再起動
+sudo systemctl restart ssh
+
+# fin
