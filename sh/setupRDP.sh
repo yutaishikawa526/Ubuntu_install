47fcc9d505f312dc4dc5c03d924338faf4150ffc
yutaishikawa526 <yutaishikawa526@gmail.com>
2024-03-01 12:41:45 +0900

    disable xrdp auto exec

--- sh/setupRDP.sh
+++ sh/setupRDP.sh
@@ -11,3 +11,7 @@ sudo ufw allow from '10.8.0.0/24' to any port 3389 proto tcp
 sudo ufw enable
 
 sudo systemctl stop xrdp
+sudo systemctl stop xrdp-sesman
+
+sudo systemctl disable xrdp
+sudo systemctl disable xrdp-sesman
