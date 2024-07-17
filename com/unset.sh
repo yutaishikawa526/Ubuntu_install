e9f4ae4de0f1c4956563c446e13e6eb860b24e3d
yutaishikawa526 <yutaishikawa526@gmail.com>
2024-07-17 23:27:18 +0900

    軽微な修正

--- com/unset.sh
+++ com/unset.sh
@@ -7,10 +7,10 @@ source "$_DIR/conf/conf.sh"
 source "$_DIR/conf/conf_mnt.sh"
 source "$_DIR/com/com.sh"
 
-sudo umount "$_MNT_POINT/sys" || true
-sudo umount "$_MNT_POINT/dev/pts" || true
-sudo umount "$_MNT_POINT/dev" || true
-sudo umount "$_MNT_POINT/proc" || true
-sudo umount "$_MNT_POINT/boot/efi" || true
-sudo umount "$_MNT_POINT/boot" || true
-sudo umount "$_MNT_POINT" || true
+sudo umount "$_MNT_POINT/sys" 2> /dev/null || true
+sudo umount "$_MNT_POINT/dev/pts" 2> /dev/null || true
+sudo umount "$_MNT_POINT/dev" 2> /dev/null || true
+sudo umount "$_MNT_POINT/proc" 2> /dev/null || true
+sudo umount "$_MNT_POINT/boot/efi" 2> /dev/null || true
+sudo umount "$_MNT_POINT/boot" 2> /dev/null || true
+sudo umount "$_MNT_POINT" 2> /dev/null || true
